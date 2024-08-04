34de9ee8e2968ee7593bda5541b0a9c8e5d59379
nayera540 <1900205@eng.asu.edu.eg>
2024-08-04 21:35:27 +0300

    Add projects component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeroSectionComponent } from './components/hero-section/hero-section.component';
+import { AboutmeComponent } from './components/aboutme/aboutme.component';
+import { SkillsSectionComponent } from './components/skills-section/skills-section.component';
+import { ProjectsComponent } from './components/projects/projects.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeroSectionComponent, AboutmeComponent, SkillsSectionComponent, ProjectsComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
